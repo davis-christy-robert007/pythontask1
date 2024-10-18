69a7dabfff62dac04f533f9ab3ee490ba873cc5a
davis-christy-robert007 <daviscr2006@gmail.com>
2024-10-18 15:10:12 +0530

    Add files via upload

---
+++ pythontask2.py
@@ -0,0 +1,10 @@
+'''
+Author:Davis christy Robert
+Date:18-10-2024
+Descripition: Python program that stores a string in a variable.
+Extract a specific part of the string (substring) and then concatenate it with another string.
+Finally, display the new string.
+'''
+string1="Hello, World!"
+string2=string1[6:12]
+print(string2+ " Everyone!")
