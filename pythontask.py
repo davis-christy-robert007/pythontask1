6ba5bc984a54121898673d0c1998ef006ccb1d00
davis-christy-robert007 <daviscr2006@gmail.com>
2024-10-18 14:26:45 +0530

    Add files via upload

---
+++ pythontask.py
@@ -0,0 +1,10 @@
+'''
+Author: Davis christy robert
+Date:18-10-2024
+Description:a Python program that asks the user to input their name and age.
+After receiving the input, display a message that includes the user's name and age.
+Hello, John! You are 25 years old.
+'''
+name=input("enter your name")
+age= input("entewr your age")
+print("Hello,",name,"\b! You are",age,"years old")
