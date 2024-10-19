84bffc7687c3afaf8047a9b42be09c6f81a8764c
davis-christy-robert007 <daviscr2006@gmail.com>
2024-10-19 15:05:25 +0530

    Add files via upload

---
+++ PYTHONTASK6.py
@@ -0,0 +1,19 @@
+"""
+Author:Davis Christy Robert
+Date:19-10-
+"""
+num1=int(input("enter the number"))
+num2=int(input("enter the number"))
+num3=int(input("enter the number"))
+addition=num1+num2
+print("The sum of num1 and num2 is:",addition)
+Subtraction=num2-num1
+print("The difference when num2 is subtracted from num1 is",Subtraction)
+Multiplication=num1*num2
+print("The product of num1 and num2 is",Multiplication)
+Division=num1/num2
+print(" The quotient when num1 is divided by num2 is",Division)
+Modulus=num1%num2
+print("The remainder when num1 is divided by num2 is",Modulus)
+combining=(num1 + num2) * num3 /2
+print("The result of (num1 + num2) * num3 / 2 is",combining)
