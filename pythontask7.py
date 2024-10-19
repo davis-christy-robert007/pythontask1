7828c45b5c5200e3fa0f221d3f396e15fb751785
davis-christy-robert007 <daviscr2006@gmail.com>
2024-10-19 15:53:48 +0530

    Add files via upload

---
+++ pythontask7.py
@@ -0,0 +1,10 @@
+"""
+Author:Davis Christy Robert
+Date:19-10-2024
+description:Create, concatenate, and print a string and access a sub-string from a given string.
+"""
+firstname=input("enter your name")
+lastname=input("enter your name")
+fullname=firstname + " "+lastname
+print(fullname)
+print(fullname[5:])
