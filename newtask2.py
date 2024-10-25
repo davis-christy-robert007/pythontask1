2562725c442d3143981abc3886a27a6a8b6d1046
davis-christy-robert007 <daviscr2006@gmail.com>
2024-10-25 14:43:29 +0530

    Add files via upload

---
+++ newtask2.py
@@ -0,0 +1,12 @@
+"""
+Author:Davis Christy Robert
+Date:25-10-2024
+description:python program to check whether a number is postive or negative or zero
+"""
+number=int(input("enter the number"))
+if number>0:
+    print(number," is a postive number")
+elif number<0:
+    print(number,"is a negative number")
+else:
+    print(number,"is a zero")
