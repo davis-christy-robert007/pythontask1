e7587a7ca230392ba2c933a9e7c7cdec2559c603
davis-christy-robert007 <daviscr2006@gmail.com>
2024-10-25 14:16:52 +0530

    Add files via upload

---
+++ pythoN5.py
@@ -0,0 +1,10 @@
+"""
+Author:davis christy robert
+date:18-10-20
+description: Python program to print the following string in a specific format (see the output).
+Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high,
+ Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
+"""
+print("Twinkle, twinkle, little star, "
+      "How I wonder what you are!\n\t\t\t Up above the world so high, "
+      "Like a diamond in the sky.\n Twinkle, twinkle, little star,\n\t\t How I wonder what you are")
