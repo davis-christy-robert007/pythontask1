c7abcc030120cfe80a400e37aa5711b885e17bd4
Davis Christy Robert <daviscr2006@gmail.com>
2024-12-12 00:11:17 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -9,12 +9,12 @@ Date:
 def fibonacci(n):
     a=0
     b=1
-    #print(a)
-    #print(b)
+    
+    
     for i in range(n-2):
         x=a+b
         print(x)
         a=b
         b=x
 
-fibonacci(10)
+
